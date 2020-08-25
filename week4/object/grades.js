1d172aea30d6818adc50f0ffbf662d5f21e35718
lch5490 <lch950223@gmail.com>
2020-08-25 16:12:26 +0900

    인스타그램 윗부분까지

---
+++ week4/object/grades.js
@@ -0,0 +1,8 @@
+var grades = {
+  list: { egoing: 10, k8805: 6, sorialgi: 80 },
+  show: function () {
+    for (var name in this.list) {
+      document.write(name + ": " + this.list[name] + "<br />");
+    }
+  },
+};
